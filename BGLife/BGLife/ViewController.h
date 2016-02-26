e6b95b72a9e2be2a734a2879ceed4f812f12e1e2
zhangsp1306 <zhangsp1306@163.com>
2016-02-26 14:32:01 +0800

    kabfaifni

---
+++ BGLife/BGLife/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BGLife
+//
+//  Created by zsp on 16/2/26.
+//  Copyright © 2016年 张书鹏. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ViewController : BaseViewController
+
+@end
---
+++ BGLife/BGLife/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  BGLife
+//
+//  Created by zsp on 16/2/26.
+//  Copyright © 2016年 张书鹏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
